230c7b1305456f11d74cabe4c79eceb6ff563c76
F.Ogata <82166626+wolflars@users.noreply.github.com>
2021-08-03 21:09:07 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//jshint esversion:6 
+
+const superheroes = require('superheroes');
+
+let myName = superheroes.random()
+
+
+
+const supervillains = require('supervillains');
+
+let yourName = supervillains.random();
+
+console.log("Meu nome de herói é " + myName + " meu nome de vilão é " +  yourName);
